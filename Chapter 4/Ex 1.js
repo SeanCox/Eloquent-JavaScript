dcfff32e11d25169c0a0da9851dfb426c576bd5d
SeanCox <sean.cox@mac.com>
2016-02-09 10:25:30 -0800

    Finish Ch 4, Ex 1

--- Chapter 4/Ex 1.js
+++ Chapter 4/Ex 1.js
@@ -1,1 +1,30 @@
-console.log('hello world');
+var range = function(start, end, by){
+  var array = [];
+  var placeHolder;
+
+  if (by === undefined) by = 1;
+
+  if (by > 0){
+    for(start; start <= end; start += by){
+      array.push(start);
+    }
+  }else{
+    for(start; start >= end; start += by){
+      array.push(start);
+    }
+  }
+    return array;
+  }
+
+  var sum = function(array){
+    var total = 0;
+
+    for (var i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+    return total;
+  }
+
+  console.log(range(1, 10));
+  console.log(range(5, 2, -1));
+  console.log(sum(range(1, 10)));
