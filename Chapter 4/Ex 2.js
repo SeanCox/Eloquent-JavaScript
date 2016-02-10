4620f3dfd04c34aad3ddbb06f7efbf43626a7fd6
SeanCox <sean.cox@mac.com>
2016-02-10 15:30:15 -0800

    Finish Ch4 Ex2

--- Chapter 4/Ex 2.js
+++ Chapter 4/Ex 2.js
@@ -1,7 +1,7 @@
 var reverseArray = function(array){
   var newArray = [];
 
-  for (var i = array.length; i >= 0; i--) {
+  for (var i = array.length -1; i >= 0; i--) {
     newArray.push(array[i]);
   }
   return newArray;
@@ -9,3 +9,18 @@ var reverseArray = function(array){
 
 
 console.log(reverseArray(['a','b','c']));
+
+var reverseArrayInPlace = function(array){
+  var hold;
+  for(i = 0; i < Math.floor(array.length/2); i++){
+    hold = array[i];
+    array[i] = array[array.length - i -1];
+    array[array.length - i -1] = hold;
+  }
+  return array;
+}
+
+var arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
