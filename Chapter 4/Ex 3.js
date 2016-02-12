71e3ee7c73f4a9ee2a07f4832713a207e31e65a6
SeanCox <sean.cox@mac.com>
2016-02-12 12:42:15 -0800

    continue ch4 ex3

--- Chapter 4/Ex 3.js
+++ Chapter 4/Ex 3.js
@@ -1,11 +1,26 @@
 var arrayToList = function(array){
-  var list = {};
+  var list = null;
 
-
-  array.forEach(function(i){
-    list.push({'value': i.value,})
-  })
-
-
+  for(i = array.length-1; i >= 0; i-- ){
+   list = {Value: array[i], rest: list}
+  }
   return list;
+}
+
+var listToArray = function(list){
+    var array = [];
+    //need stuff here
+    return array;
+}
+
+
+var prepend = function(element, list){
+  var newList
+  newList = {Value: array[i], rest: list}
+  return newList;
+}
+
+var nth = function(list, number){
+//need stuff here
+
 }
