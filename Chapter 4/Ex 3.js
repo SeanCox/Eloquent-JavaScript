d0797dcd55ab1c27b67d6974ccd737e4415b980d
SeanCox <sean.cox@mac.com>
2016-02-12 17:34:57 -0800

    Fix ch4 ex3
    Use book to sort out mistakes.

--- Chapter 4/Ex 3.js
+++ Chapter 4/Ex 3.js
@@ -1,26 +1,41 @@
 var arrayToList = function(array){
   var list = null;
 
-  for(i = array.length-1; i >= 0; i-- ){
-   list = {Value: array[i], rest: list}
+  for(var i = array.length-1; i >= 0; i-- ){
+   list = {value: array[i], rest: list}
   }
   return list;
 }
 
 var listToArray = function(list){
     var array = [];
-    //need stuff here
+for (var node = list; node; node = node.rest) {
+  array.push(node.value);
+}
     return array;
 }
 
 
 var prepend = function(element, list){
-  var newList
-  newList = {Value: array[i], rest: list}
+  var newList;
+  newList = {Value: element, rest: list}
   return newList;
 }
 
 var nth = function(list, number){
-//need stuff here
-
+  if (!list)
+    return undefined;
+  else if (number == 0)
+    return list.value;
+  else
+    return nth(list.rest, number - 1);
 }
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
