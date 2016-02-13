b3da57d99f727d6a3bc604a93f92706dd67c1503
SeanCox <sean.cox@mac.com>
2016-02-13 11:17:42 -0800

    work on ch4 ex4

---
+++ Chapter 4/Ex 4.js
@@ -0,0 +1,14 @@
+var deepEqual = function(varA, varB){
+  if(varA === varB){
+  return true;
+}else {
+  return false;
+}
+}
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
